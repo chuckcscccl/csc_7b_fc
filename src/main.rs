98a50aba5b39a258b5ab74022f78fed9f9b97f1c
Chuck Liang <chuckcscccl@gmail.com>
2024-04-20 16:24:43 -0400

    cargo fmt applied

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
 #![allow(dead_code)]
-use csc_7b_fc::*;
 use csc_7b_fc::Expr::*;
+use csc_7b_fc::*;
 
+pub fn compile(e: &Expr) {
+    match e {
+        Val(x) => println!("push {}", x),
+        Neg(a) => {
+            compile(a);
+            println!("push 0");
+            println!("pop bx"); // moves 0 into bx register
+            println!("pop ax"); // load result for a into ax
+            println!("sub ax bx"); // this instruction has semantics bx -= ax
+            println!("push bx"); // always push result (0-a) on stack
+        }
+        _ => {
+            eprintln!("ERROR: COMPILATION FAILED;");
+            return;
+        }
+    } //match
+} //compile
 
-pub fn compile(e:&Expr) {
-  match e {
-    Val(x) => println!("push {}",x),
-    Neg(a) => {
-       compile(a);
-       println!("push 0");
-       println!("pop bx");     // moves 0 into bx register
-       println!("pop ax");     // load result for a into ax
-       println!("sub ax bx");  // this instruction has semantics bx -= ax
-       println!("push bx");    // always push result (0-a) on stack 
-    },
-    _ => {
-       eprintln!("ERROR: COMPILATION FAILED;");
-       return;
-   },
-  }//match
-}//compile
-
-
-
-fn main()  // this version of main compiles
+fn main() // this version of main compiles
 {
-   //print!("Enter Expression: ");  // don't echo to output
-   let mut input = String::from("0");
-   std::env::args().nth(1).map(|s|{ input = String::from(s); });
-   let tokens = lex(&input[..].trim());
-   let exp = parse(&tokens);
-   exp.map(|e|{ compile(&e); });
-}//main   
+    //print!("Enter Expression: ");  // don't echo to output
+    let mut input = String::from("0");
+    std::env::args().nth(1).map(|s| {
+        input = String::from(s);
+    });
+    let tokens = lex(&input[..].trim());
+    let exp = parse(&tokens);
+    exp.map(|e| {
+        compile(&e);
+    });
+} //main
